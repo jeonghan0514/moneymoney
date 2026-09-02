771d327d572bff8edea2ba146760ccc63cb5b8f7
jeonghan0514 <jeonghan0514@gmail.com>
2026-09-02 17:22:36 +0800

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  base: '/moneymoney/' // 這裡設定跟你的 GitHub 專案名稱一樣
+  base: '/moneymoney/'
 })
